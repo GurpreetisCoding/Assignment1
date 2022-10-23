a2ba5229af5a35298785727b9077ceee291bcf0a
Gurpreet Singh <113737546+GurpreetisCoding@users.noreply.github.com>
2022-10-23 02:00:13 +0000

    I changed the grid layout adding "aside" tag to the pages. I made the site resizable using @media for better experience for mobile users.

--- script.js
+++ script.js
@@ -5,10 +5,35 @@ function validateForm() {
     alert("Passwords dont match");
     return false;
   }
-  let name = document.forms["theForm"]["user"].value;
-  const existingUser = ["Harry", "Elizabeth", "Shana"];
-  if (existingUser.includes(name) == true) {
+
+  let name = document.forms["theForm"]["user"].value; //this line finds out the value of user from "theForm" in page3. 
+  const existingUser = ["Harry", "Elizabeth", "Shana"]; //arraylist 
+  if (existingUser.includes(name) == true) { 
     alert("Username already exists");
     return false;
+  }
+}
+
+function drawShape() {
+  // This lines gets the canvas that we are working on
+  var canvas = document.getElementById('mycanvas');
+            
+  // Sees if the browser supports canvas 
+  if (canvas.getContext) {
+            
+    // This allows you to use the canvas and specify the rending
+    var ctx = canvas.getContext('2d');
+               
+    ctx.fillStyle = '#FF0000';
+    ctx.font = 'Italic 30px Sans-Serif';
+               
+    ctx.textBaseline = 'Top';
+    ctx.fillText('JavaScript is Cool!', 40, 100);
+
+    ctx.strokeStyle = '#00F';
+    ctx.font = 'Bold 25px Sans-Serif';
+    ctx.strokeText('Welcome to Page1!', 40, 60);
+  } else {
+    alert('You need Safari or Firefox 1.5+ to see this demo.');
   }
 }
